e56cde42c5d459921ab8495ca990bb94eb4eaaad
vidy <120648590@qq.com>
2017-03-12 22:40:54 +0800

    初始化项目

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {IpcRendererService} from './shared/ipc-renderer.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [IpcRendererService]
 })
 export class AppComponent {
   title = 'app works!';
+  constructor (private IpcRendererService) {
+    this.IpcRendererService.send('msg', 'halo');
+    this.IpcRendererService.on('msgFromMain', (msg) => {
+      (console.log(msg));
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import {AppRoutingModule} from './app.routing';
 
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    SharedModule
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
